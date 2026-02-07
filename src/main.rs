4d7d593068fcb0eec15a84e1aae7128b90457a1a
javier ruben echeverria <javier.ruben.echeverria@gmail.com>
2026-02-07 14:43:21 -0500

    Writing cpu cycle logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use crate::chip_8::Chip8;
-use create::instruction_loader::load_rom;
+use crate::instruction_manager::load_rom;
 
 
 mod chip_8;
-mod instruction_loader;
+mod constants;
+mod utility;
 
 fn main() {
-    let mut chip_8 = Chip8::new(); // loads default memory configuration
-    load_rom(chip_8);
+    let mut chip8 = Chip8::new(); // loads default memory configuration
+    load_rom();
     
-    // Run main CPU loop 
     loop {
         fetch();
         decode();
